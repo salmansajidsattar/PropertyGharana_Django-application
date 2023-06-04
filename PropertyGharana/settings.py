1b6176291efd58b4de3bb4192e258686541f1b04
Salman Sajid <salmansajidsattar@gmail.com>
2023-06-04 23:19:04 +0500

    timezone updated

--- PropertyGharana/settings.py
+++ PropertyGharana/settings.py
@@ -114,11 +114,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-# TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
-USE_I18N = True
+# USE_I18N = True
 
-USE_L10N = True
+# USE_L10N = True
 
 # USE_TZ = True
 
